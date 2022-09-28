a40fde049a4bb806381e7bee0acf468344fa8500
RUslan <ruslan56781@mail.ru>
2022-09-28 21:05:36 +0300

    Client

---
+++ Client/include/mainwindow.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTcpSocket>
+#include <QMessageBox>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+     void SetUpServer();
+
+public slots:
+    void connectToServer();
+    void SendMessageToServer();
+    void AcceptMessageFromServer();
+
+
+private slots:
+    void on_lineEdit_3_returnPressed();
+
+private:
+    QTcpSocket *m_clientSocket;
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
---
+++ Client/src/mainwindow.cpp
@@ -0,0 +1,48 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+  this->setWindowTitle("Client");
+  SetUpServer();
+}
+
+MainWindow::~MainWindow() { delete ui; }
+
+void MainWindow::SetUpServer() {
+  m_clientSocket = new QTcpSocket(this);
+  connect(ui->pushButton_2, SIGNAL(clicked()), this, SLOT(connectToServer()));
+  connect(m_clientSocket, SIGNAL(disconnected()), this, SLOT(deleteLater()));
+  connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(SendMessageToServer()));
+}
+
+void MainWindow::connectToServer() {
+  m_clientSocket->abort();
+  m_clientSocket->connectToHost(ui->lineEdit_2->text(),
+                                ui->lineEdit->text().toInt());
+  connect(m_clientSocket, SIGNAL(readyRead()), this,
+          SLOT(AcceptMessageFromServer()));
+}
+// send message buttons
+void MainWindow::SendMessageToServer() {
+  QString message = ui->lineEdit_3->text();
+  m_clientSocket->write(ui->lineEdit_3->text().toLatin1());
+  message = "You: " + message;
+  ui->textEdit->append(message);
+  ui->lineEdit_3->clear();
+}
+
+void MainWindow::on_lineEdit_3_returnPressed() {
+  QString message = ui->lineEdit_3->text();
+  m_clientSocket->write(ui->lineEdit_3->text().toLatin1());
+  message = "You: " + message;
+  ui->textEdit->append(message);
+  ui->lineEdit_3->clear();
+}
+
+void MainWindow::AcceptMessageFromServer() {
+  QString message = m_clientSocket->readAll();
+  message = "Server:" + message;
+  ui->textEdit->append(message);
+}
